815939f72ce72e0f30bf6a574e458ae4ea94cffe
Maxwell Geringer <>
2021-04-26 20:53:31 +0000

    Created HTML/EJS files

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const DatabaseClient= require("@replit/database");
 const database = new DatabaseClient();
@@ -13,3 +17,20 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('server started');
 });
+
+app.post('/login', (req, res) => {
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
+});
+
+async function createAccount(username, name, pass) {
+  let keyLocation = "users." + id + ".";
+  await database.set(keyLocation + "username", username);
+  await database.set(keyLocation + "name", name);
+  await database.set(keyLocation + "pass", pass);
+  await database.set(keyLocation + "creation", Date());
+  await database.set(keyLocation + "pfpUrl", "");
+  await database.set(keyLocation + "admin", "false");
+}
+
+app.use("/", router);
