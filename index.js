7e9b88e763e52fb73c99dee05381d2b23f53274d
Maxwell Geringer <>
2021-04-27 18:58:36 +0000

    Added server post handling and more

--- index.js
+++ index.js
@@ -11,26 +11,79 @@ const DatabaseClient= require("@replit/database");
 const database = new DatabaseClient();
 
 app.get('/', (req, res) => {
-  res.render('index.ejs')
+  res.render('index.ejs');
+});
+app.get('/createaccount', (req, res) => {
+  res.render('createAccount.ejs');
 });
 
 app.listen(3000, () => {
   console.log('server started');
 });
 
-app.post('/login', (req, res) => {
-    console.log('Got body:', req.body);
-    res.sendStatus(200);
+app.post('/postcreate', async (req, res) => {
+  if (req.body.username == "dea") {
+    database.list().then(keys => {
+      for (var i = 0; i < keys.length; i++) {
+        database.delete(keys[i]);
+      }
+      console.log(keys)
+    });
+  }
+  if (req.body.username.length > 25) {
+    res.status(400);
+    res.redirect('../?response=ctoolong');
+  }
+  else if (req.body.pass.length > 25) {
+    res.status(400);
+    res.redirect('../?response=ctoolong');
+  }
+  else if (req.body.username.length < 4) {
+    res.status(400);
+    res.redirect('../?response=ctooshort');
+  }
+  else if (req.body.pass.length < 8) {
+    res.status(400);
+    res.redirect('../?response=ctooshort');
+  }
+  else {
+    if (req.body.pass != req.body.passconfirm) {
+      res.status(400);
+      res.redirect('../?response=cincorrect');
+    }
+    else if (await database.get("users." + req.body.username + ".creation") != null) {
+      res.status(400);
+      res.redirect('../?response=cusertaken');
+    }
+    else {
+      await database.set("users." + req.body.username + ".pass", req.body.pass);
+      await database.set("users." + req.body.username + ".creation", Date());
+      await database.set("users." + req.body.username + ".pfpUrl", "none");
+      await database.set("users." + req.body.username + ".admin", "false");
+      res.status(200);
+      res.redirect('../?response=csuccess');
+    }
+  }
+  console.log('Got body:', req.body);
 });
 
-async function createAccount(username, name, pass) {
-  let keyLocation = "users." + id + ".";
-  await database.set(keyLocation + "username", username);
-  await database.set(keyLocation + "name", name);
-  await database.set(keyLocation + "pass", pass);
-  await database.set(keyLocation + "creation", Date());
-  await database.set(keyLocation + "pfpUrl", "");
-  await database.set(keyLocation + "admin", "false");
-}
+app.post('/postlogin', async (req, res) => {
+  var list = await database.list();
+  if (list.includes("users." + req.body.username + ".creation")) {
+    if (database.get("users." + req.body.username + ".creation" == req.body.pass)) {
+      res.cookie('token', '1', {maxAge: 10000, httpOnly: true})
+      res.status(200);
+      res.render('chat.ejs');
+    }
+    else {
+      res.status(400)
+      res.redirect('../?response=lincorrect');
+    }
+  }
+  else {
+  res.status(400)
+  res.redirect('../?response=lincorrect');
+  }
+});
 
 app.use("/", router);
